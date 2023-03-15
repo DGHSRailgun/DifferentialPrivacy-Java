4bff7de6a6c2ae6072d8581eb2494ee10f31d0ea
BuildTools <unconfigured@null.spigotmc.org>
2023-03-14 21:28:01 -0400

    build Bounded Mean

---
+++ src/main/java/org/example/Scenarios/CalculateBoundedMean.java
@@ -0,0 +1,15 @@
+package org.example.Scenarios;
+
+public class CalculateBoundedMean {
+
+    private static final String NON_PRIVATE_OUTPUT = "non_private_bounded_mean_per_day.csv";
+    private static final String PRIVATE_OUTPUT = "private_bounded_variance_mean_day.csv";
+    private static final double LN_3 = Math.log(3);
+
+
+
+
+
+
+
+}
